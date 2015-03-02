4c01e0f0c57a04da16e210a26ef743509ab3db08
Cesar Toscano <ctoscano@bab.as>
2015-03-02 11:36:04 -0800

    added location bar to chrome-flat

--- app/scripts/directives/embeddeck.js
+++ app/scripts/directives/embeddeck.js
@@ -10,12 +10,15 @@ angular.module('campaignntApp')
   .directive('embedDeck', ['$location', function ($location) {
     return {
       template: '<iframe frameborder="0"></iframe>',
+      scope : { target : '=' },
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         var target = 'http://ntoscano.github.io/membrightVideoPlayer/app/index.html';
         var deckId = $location.search().deck;
         if (deckId) target += '#?deck=' + deckId;
         element.find('iframe').attr('src', target);
+
+        scope.target = target;
       }
     };
   }]);
