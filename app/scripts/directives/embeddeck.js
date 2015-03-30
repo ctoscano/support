a1a8405451131a83cbca9a587d24801d950a0fe8
Cesar Toscano <ctoscano@bab.as>
2015-03-30 16:32:14 -0700

    code cleanup

--- app/scripts/directives/embeddeck.js
+++ app/scripts/directives/embeddeck.js
@@ -7,13 +7,13 @@
  * # embedDeck
  */
 angular.module('campaignntApp')
-  .directive('embedBrowser', ['$location', function ($location) {
+  .directive('embedBrowser', [function () {
     return {
       template: '<a href="{{target}}" target="_blank" class="location">{{target}}</a><div><iframe frameborder="0" ></iframe></div>',
       transclude: true,
       scope : { target : '@' },
       restrict: 'C',
-      link: function postLink(scope, element, attrs) {
+      link: function postLink(scope, element) {
         element.find('iframe').attr('src', scope.target);
       }
     };
@@ -23,10 +23,10 @@ angular.module('campaignntApp')
       template: '<iframe frameborder="0"></iframe>',
       scope : { target : '=' },
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
+      link: function postLink(scope, element) {
         var target = 'http://ntoscano.github.io/membrightVideoPlayer/app/index.html';
         var deckId = $location.search().deck;
-        if (deckId) target += '#?deck=' + deckId;
+        if (deckId) { target += '#?deck=' + deckId; }
         element.find('iframe').attr('src', target);
 
         scope.target = target;
